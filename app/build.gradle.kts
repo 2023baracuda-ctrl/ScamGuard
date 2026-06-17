908057845bfc921783da1c62d1ac33c864ee1415
2023baracuda-ctrl <2023baracuda@gmail.com>
2026-06-17 09:25:42 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "md.scamguard"
     minSdk = 26
     targetSdk = 36
-    versionCode = 1.1
-    versionName = "1.0"
+    versionCode = 1
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
